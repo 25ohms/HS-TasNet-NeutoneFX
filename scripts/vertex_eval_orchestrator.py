eb828aa9291f2e8ca8d9a1bc6bee03d3f1b588fe
Sahal Sajeer Kalandan <sahal@nuanse.ca>
2026-03-11 01:11:48 -0400

    removed redundancies

--- scripts/vertex_eval_orchestrator.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-from hs_tasnet.vertex_eval_orchestrator import main
-
-
-if __name__ == "__main__":
-    main()
--- scripts/vertex_orchestrator.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-from hs_tasnet.vertex_orchestrator import main
-
-
-if __name__ == "__main__":
-    main()
